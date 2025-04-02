3cfef23a1f848f0e935d7135a465df13d3dd033e
marshmallowing <yuin1111801@naver.com>
2025-04-02 19:59:30 +0900

    Chore: Application 코드 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
-package racingcar;;
+package racingcar;
+
+import racingcar.domain.RacingGame;
+import racingcar.service.RacingGameService;
 
 public class Application {
     public static void main(String[] args) {
-
+        RacingGameService racingGameService = new RacingGameService();
+        RacingGame game = racingGameService.startGame();
+        racingGameService.playGame(game);
     }
 }
